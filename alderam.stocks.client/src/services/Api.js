7b268c9f14d891bd46174e814028e86f5bddd60f
Alex Ramos <alexdefaro@hotmail.com>
2020-04-21 10:08:24 +0100

    concertando endereco da API

--- alderam.stocks.client/src/services/Api.js
+++ alderam.stocks.client/src/services/Api.js
@@ -1,6 +1,6 @@
 ﻿import axios from "axios";
  
 //const Api = axios.create({ baseURL: "https://localhost:44330/api/" })
-const Api = axios.create({ baseURL: "https://alderamstocksapi.azurewebsites.net" })
+const Api = axios.create({ baseURL: "https://alderamstocksapi.azurewebsites.net/api/" })
 
 export default Api;
