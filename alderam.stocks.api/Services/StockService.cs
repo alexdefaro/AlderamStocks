1ee1360fe61831994df133f88cc39d58cc40cfd9
Alex Ramos <alexdefaro@hotmail.com>
2021-02-11 15:43:26 +0000

    Acertando novo calculo de corretagem

--- alderam.stocks.api/Services/StockService.cs
+++ alderam.stocks.api/Services/StockService.cs
@@ -506,7 +506,7 @@ namespace alderam.stocks.api.Services
 
         public decimal CalcularCorretagem(decimal taxaDaCoretagem, IEnumerable<Operacao> operacoes)
         {
-            /* Old calculation rule = (10+((F5)*0.003))+F6 */ 
+            /* Old calculation rule = (10+((F5)*0.003))+F6 */
             // var valorDaOperacao = operacoes.Where(r => r.Ativo.TipoDeInvestimento == TiposDeInvestimento.Acao).Sum(o => (o.PrecoUnitario * o.Quantitidade));
 
             // if (valorDaOperacao == 0)
@@ -516,11 +516,8 @@ namespace alderam.stocks.api.Services
 
             // var result = (10 + (Math.Abs(valorDaOperacao) * 0.003m)) + taxaDaCoretagem;
 
-            /* New calculation rule = (Number od operations * 4.70) */ 
-             var result = operacoes.Count() * taxaDaCoretagem
-
-            if (valorDaOperacao < 0)
-                result = -result;
+            /* New calculation rule = (Number od operations * 4.70) */
+            var result = operacoes.Count() * taxaDaCoretagem;
 
             return Truncate((decimal)result);
         }
